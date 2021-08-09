1ed31284d342e393b1fdabc41cd6e1ebadad3a6f
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-08-09 19:45:55 +0000

    Refactored

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 - https://css-tricks.com/updating-a-css-variable-with-javascript/
 - Use regex to extract numbers from mixed string: https://stackoverflow.com/questions/42532450/extract-number-from-string-javascript
+
+```js
+const extractNum = (str) => {
+  return (str.match(/\d+/g) || []).map(n => parseInt(n)).shift();
+}
+```
--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-const doc = document.documentElement;
 const $ = (el) => document.querySelector(el);
+const doc = document.documentElement;
 const setCssVar = (prop, value) => doc.style.setProperty(prop, value);
 const getCSSVar = (prop) => getComputedStyle(doc).getPropertyValue(prop).trim();
 
-const initialSettings = {}
+const initialSettings = {};
 const props = ['speed', 'rotation', 'origin-x', 'origin-y'];
 
-props.forEach(prop => initialSettings[prop] = getCSSVar('--' + prop));
-
-$('#play').addEventListener('click', (e) => {
-  const running = getCSSVar('--play-state') === 'running';
-  const toggledState = running ? 'paused' : 'running';
-  const buttonLabel = running ? 'Play' : 'Pause';
-  setCssVar('--play-state', toggledState);
-  e.target.textContent = buttonLabel;
+props.forEach(prop => {
+  const input = $(`#${prop}`);
+  initialSettings[prop] = { value:input.value, unit:input.dataset.unit }
+  $(`#${prop}`).addEventListener('input', (e) => {
+    setCssVar(`--${prop}`, e.target.value + input.dataset.unit);
+  });
 });
 
 $('#reset').addEventListener('click', (e) => {
   props.forEach(prop => {
-    const val = initialSettings[prop];
-    setCssVar('--' + prop, val);
-    $(`#${prop}`).value = extractNum(val);
+    const { value, unit } = initialSettings[prop];
+    setCssVar('--' + prop, value + unit);
+    $(`#${prop}`).value = value;
   });
 });
 
-const extractNum = (str) => {
-  return (str.match(/\d+/g) || []).map(n => parseInt(n)).shift();
-}
-
-$('#speed').addEventListener('input', (e) => {
-  setCssVar('--speed', e.target.value + 's');
-});
-
-$('#rotation').addEventListener('input', (e) => {
-  setCssVar('--rotation', e.target.value + 'deg');
-});
-
-$('#origin-x').addEventListener('input', (e) => {
-  setCssVar('--origin-x', e.target.value + '%');
-});
-
-$('#origin-y').addEventListener('input', (e) => {
-  setCssVar('--origin-y', e.target.value + '%');
+$('#play').addEventListener('click', (e) => {
+  const running = getCSSVar('--play-state') === 'running';
+  const toggledState = running ? 'paused' : 'running';
+  setCssVar('--play-state', toggledState);
+  e.target.textContent = running ? 'Play' : 'Pause';
 });
